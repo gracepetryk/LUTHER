b51ed93727e8d76e2b513d17ae51b0ac6f760f52
Grace Petryk <gracepetryk99@gmail.com>
2020-04-29 00:38:15 -0600

    this should be working

--- src/__main__.py
+++ src/__main__.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-from more_itertools import consume
+
+from collections import deque
 from itertools import islice
-from typing import List, Set
+from typing import List, Union
 import sys
+import re
 from dfa import DFA
 
 
@@ -26,8 +28,8 @@ class LUTHER:
                     if i in [' ', '\n', '\t']:
                         continue
                     if i == 'x':
+                        # read in the next 2 characters as a hex character code
                         self.alphabet += chr(int("".join(islice(index_range, 2)), 16))
-                        consume(header, 2)  # skip the next two characters
                     else:
                         self.alphabet.append(i)
 
@@ -47,11 +49,68 @@ class LUTHER:
         except IOError:
             sys.exit(1)
 
+    def tokenize_source(self):
+
+        # record the location of each newline in a queue so we can know what line we're on.
+        newline_indices = deque()
+        newline_indices.appendleft(-1)  # needed to make sure character line position works
+        for i, char in zip(range(len(self.input_text)), self.input_text):
+            if char == '\n':
+                newline_indices.appendleft(i)
+
+        with open(self._output_path, 'w') as o:
+            token_start_char = 1  # the line the current token starts on
+            token_start_line = 1  # the character on that line the token starts on
+            current_char_index = 0  # the character index in the input string the token starts on
+
+            while current_char_index < len(self.input_text):
+
+                # run all the dfa matching
+                for dfa in self.dfa_list:
+                    dfa.match(self.input_text, current_char_index)
+
+                # find the best dfa, tie goes to first in list
+                best_dfa: Union[DFA, None] = None
+                for dfa in self.dfa_list:
+                    if (best_dfa is None
+                            or (len(dfa.longestPreviousMatch) > 0
+                                and len(dfa.longestPreviousMatch) > len(best_dfa.longestPreviousMatch))):
+                        # if the best_dfa is undefined or the current dfa is
+                        # defined and longer than the best, update best
+                        best_dfa = dfa
+
+                # write token data to file
+                o.write(best_dfa.identifier + " ")
+
+                token = convert_escaped_chars(best_dfa.longestPreviousMatch) \
+                    if best_dfa.token is None else best_dfa.token
+                o.write(token + " ")
+
+                o.write("{} {}".format(token_start_line, token_start_char))
+                o.write("\n")
+
+                # figure out the start character and line of next token
+                current_char_index += len(best_dfa.longestPreviousMatch)
+
+                last_newline = newline_indices.pop()
+                next_newline = newline_indices.pop()
+
+                while current_char_index > next_newline:
+                    last_newline = next_newline
+                    next_newline = newline_indices.pop() if newline_indices else len(self.input_text)
+                    token_start_line += 1
+
+                newline_indices.append(next_newline)
+                newline_indices.append(last_newline)
+
+                # plus one because current char and last newline are both 0 indexed while token start is 1 indexed
+                token_start_char = current_char_index - last_newline
+
     def __read_dfa(self, line: str) -> DFA:
         params = line.split()
         file_name = params[0]
         identifier = params[1]
-        token = params[2] if len(params) > 2 else None
+        token = params[2] if len(params) > 2 else None  # only read in token if provided
 
         try:
             with open(file_name, 'r') as file:
@@ -61,10 +120,28 @@ class LUTHER:
             sys.exit(1)
 
 
+def convert_escaped_chars(input_str: str) -> str:
+    """
+    converts a regular string with escaped chars and spaces to fit the LUTHER format
+    :param input_str:
+    :return:
+    """
+
+    output_str = ""
+    allowed_char = re.compile("[A-Za-wy-z0-9]")
+    for char in input_str:
+        if allowed_char.match(char):
+            output_str += char
+        else:
+            output_str += "{0:#0{1}x}".format(ord(char), 4)[1:]
+
+    return output_str
+
+
 def main():
 
     luther = LUTHER(sys.argv[1], sys.argv[2], sys.argv[3])
-    print(luther)
+    luther.tokenize_source()
 
     sys.exit()
 
--- src/dfa.py
+++ src/dfa.py
@@ -13,7 +13,6 @@ class DFA:
         """
         self._states = []
         self.alphabet = alphabet
-        self.matchState = DFAMatchState.WILL_NOT_MATCH
         self.longestPreviousMatch = ""
         self.identifier = identifier
         self.token = token
@@ -25,35 +24,36 @@ class DFA:
             transitions = line_chars[2:].copy()
             self._states.append(_DFAState(self, transitions, accepting))
 
-    def match(self, input_str):
+    def match(self, input_str, start_index=0):
         """
         gets the longest possible match for a given input string
         :param input_str:
+        :param start_index: the index to start tokenizing at (default 0)
         """
         self._reset()
 
         current_state = self._states[0]
-        index = 0
-        current_char = input_str[0]
+        index_in_input = start_index
+        current_char = input_str[start_index]
         match = ""
 
         while current_state.can_match(current_char):
             match += current_char
+            current_state = self._states[current_state.get_transition_index(current_char)]
 
             if current_state.accepting:
-                self.matchState = DFAMatchState.MATCHING
-                if len(match) > len(self.longestMatch):
+                if len(match) > len(self.longestPreviousMatch):
                     self.longestPreviousMatch = match
-            else:
-                self.matchState = DFAMatchState.WILL_NOT_MATCH
 
-            index += 1
-            current_state = self._states[index]
-            current_char = input_str[index]
+            index_in_input += 1
+            if index_in_input < len(input_str):
+                current_char = input_str[index_in_input]
+            else:
+                # return when EOF reached
+                return
 
     def _reset(self):
-        self.matchState = DFAMatchState.WILL_NOT_MATCH
-        self.longestMatch = ""
+        self.longestPreviousMatch = ""
 
 
 class DFAMatchState(Enum):
@@ -75,7 +75,10 @@ class _DFAState:
         # populate transitions for dfa state
         for transition in zip(dfa.alphabet, tt_line):
             if transition[1] != 'E':
-                self.transitions[transition[0]] = transition[1]
+                self.transitions[transition[0]] = int(transition[1])
 
     def can_match(self, char: str) -> bool:
         return char in self.transitions.keys()
+
+    def get_transition_index(self, char: str) -> int:
+        return self.transitions[char]
